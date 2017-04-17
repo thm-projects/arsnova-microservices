0f5d8610cc8f169535bd834c241f8da0fc9cab4a
Tom Käsler <tom.kaesler@mni.thm.de>
2017-04-17 12:15:33 +0200

    fix jsonprotocol for answertypes

--- shared/src/main/scala/de/thm/arsnova/shared/mappings/ChoiceAnswerJsonProtocol.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/mappings/ChoiceAnswerJsonProtocol.scala
@@ -3,6 +3,6 @@ package de.thm.arsnova.shared.mappings
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import de.thm.arsnova.shared.entities.ChoiceAnswer
 
-object ChoiceAnswerJsonProtocol extends DefaultJsonProtocol {
+object ChoiceAnswerJsonProtocol extends DefaultJsonProtocol with UUIDFormat {
   implicit val choiceAnswerFormat: RootJsonFormat[ChoiceAnswer] = jsonFormat4(ChoiceAnswer)
 }
--- shared/src/main/scala/de/thm/arsnova/shared/mappings/FreetextAnswerJsonProtocol.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/mappings/FreetextAnswerJsonProtocol.scala
@@ -3,6 +3,6 @@ package de.thm.arsnova.shared.mappings
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import de.thm.arsnova.shared.entities.FreetextAnswer
 
-object FreetextAnswerJsonProtocol extends DefaultJsonProtocol {
+object FreetextAnswerJsonProtocol extends DefaultJsonProtocol with UUIDFormat {
   implicit val freetextAnswerFormat: RootJsonFormat[FreetextAnswer] = jsonFormat5(FreetextAnswer)
 }
