f2511ecc34bcc8d711952cc90835ed22818a0723
Tom Käsler <tom.kaesler@mni.thm.de>
2017-07-14 14:39:49 +0200

    fix /session/?keyword= path

--- gateway/src/main/scala/de/thm/arsnova/gateway/api/SessionServiceApi.scala
+++ gateway/src/main/scala/de/thm/arsnova/gateway/api/SessionServiceApi.scala
@@ -19,6 +19,8 @@ import scala.util.{Failure, Success}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.pattern.ask
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.StatusCodes
 import akka.routing.RandomPool
 import akka.routing.RandomGroup
 import spray.json._
@@ -57,9 +59,12 @@ trait SessionServiceApi extends BaseApi {
       get {
         parameter("keyword") { keyword =>
           complete {
-            (sessionList ? LookupSession(keyword)).mapTo[UUID].map { id =>
-              (sessionRegion ? GetSession(id))
-                .mapTo[Session].map(_.toJson)
+            (sessionList ? LookupSession(keyword)).mapTo[SessionIdFromKeyword].map { sidFromkey: SessionIdFromKeyword =>
+              sidFromkey.id match {
+                case Some(sessionId) =>
+                  (sessionRegion ? GetSession(sessionId))
+                    .mapTo[Session].map(_.toJson)
+              }
             }
           }
         }
