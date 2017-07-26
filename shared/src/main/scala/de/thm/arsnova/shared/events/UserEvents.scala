7410c0ba4598288bbafaa9081fccfd1244446a63
Tom Käsler <tom.kaesler@mni.thm.de>
2017-07-26 11:04:27 +0200

    add event for user role management

--- shared/src/main/scala/de/thm/arsnova/shared/events/UserEvents.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/events/UserEvents.scala
@@ -1,9 +1,11 @@
 package de.thm.arsnova.shared.events
 
-import de.thm.arsnova.shared.entities.User
+import de.thm.arsnova.shared.entities.{User, SessionRole}
 
 object UserEvents {
   trait UserEvent
 
   case class UserCreated(user: User) extends UserEvent
+
+  case class UserGetsSessionRole(sessionRole: SessionRole) extends UserEvent
 }
