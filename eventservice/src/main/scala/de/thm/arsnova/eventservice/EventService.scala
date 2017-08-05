ad31157f578752b50a7cf67cc68ef1f77e07a9d4
Tom Käsler <tom.kaesler@mni.thm.de>
2017-08-05 22:10:16 +0200

    start event shard add skeleton for sessioneventactor

--- eventservice/src/main/scala/de/thm/arsnova/eventservice/EventService.scala
+++ eventservice/src/main/scala/de/thm/arsnova/eventservice/EventService.scala
@@ -1,6 +1,25 @@
 package de.thm.arsnova.eventservice
 
+import scala.concurrent.duration._
+import akka.actor.ActorPath
+import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
+import akka.persistence.journal.leveldb.SharedLeveldbJournal
+import de.thm.arsnova.shared.shards.EventShard
+
+import scala.concurrent.Await
+
 object EventService extends App {
-  
+  import Context._
+
+  val storeRef = Await.result(system.actorSelection(ActorPath.fromString("akka://ARSnovaService@127.0.0.1:8870/user/store")).resolveOne, 5.seconds)
+  SharedLeveldbJournal.setStore(storeRef, system)
+
+  ClusterSharding(system).start(
+    typeName = EventShard.shardName,
+    entityProps = SessionEventActor.props(),
+    settings = ClusterShardingSettings(system),
+    extractEntityId = EventShard.idExtractor,
+    extractShardId = EventShard.shardResolver
+  )
 }
 
---
+++ eventservice/src/main/scala/de/thm/arsnova/eventservice/SessionEventActor.scala
@@ -0,0 +1,38 @@
+package de.thm.arsnova.eventservice
+
+import java.util.UUID
+
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.duration._
+import akka.persistence.PersistentActor
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
+import akka.actor.Props
+import akka.actor.ActorRef
+import de.thm.arsnova.shared.events.SessionEventPackage
+
+import scala.concurrent.ExecutionContext
+
+object SessionEventActor {
+  def props(): Props = {
+    Props(new SessionEventActor())
+  }
+}
+
+class SessionEventActor extends PersistentActor {
+  implicit val ec: ExecutionContext = context.dispatcher
+  implicit val timeout: Timeout = 5.seconds
+
+  override def persistenceId: String = self.path.parent.name + "-" + self.path.name
+
+  // passivate the entity when no activity
+  context.setReceiveTimeout(2.minutes)
+
+  override def receiveRecover: Receive = {
+    case _ =>
+  }
+
+  override def receiveCommand: Receive = {
+    case SessionEventPackage(id, event) =>
+  }
+}
