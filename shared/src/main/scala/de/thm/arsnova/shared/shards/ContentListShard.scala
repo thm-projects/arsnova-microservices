5238e0b9c6101b438398175f364dc791050c1b3f
Tom Käsler <tom.kaesler@mni.thm.de>
2017-08-08 10:59:09 +0200

    add handling for sessioneventpackages to shards

--- shared/src/main/scala/de/thm/arsnova/shared/shards/ContentListShard.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/shards/ContentListShard.scala
@@ -2,6 +2,7 @@ package de.thm.arsnova.shared.shards
 
 import akka.actor.{ActorRef, Props}
 import akka.cluster.sharding.ShardRegion
+import de.thm.arsnova.shared.events.SessionEventPackage
 import de.thm.arsnova.shared.servicecommands.ContentCommands.ContentCommand
 
 object ContentListShard {
@@ -9,9 +10,11 @@ object ContentListShard {
 
   val idExtractor: ShardRegion.ExtractEntityId = {
     case cmd: ContentCommand => (cmd.sessionid.toString, cmd)
+    case event: SessionEventPackage => (event.id.toString, event)
   }
 
   val shardResolver: ShardRegion.ExtractShardId = {
     case cmd: ContentCommand => math.abs(cmd.sessionid.hashCode() % 100).toString
+    case event: SessionEventPackage => math.abs(event.id.hashCode() % 100).toString
   }
 }
