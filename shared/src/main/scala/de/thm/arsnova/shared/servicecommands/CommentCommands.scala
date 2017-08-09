be012bc4ad5730897ab558771f6950d062297515
Tom Käsler <tom.kaesler@mni.thm.de>
2017-08-09 14:09:09 +0200

    prepare comment commands for sharding

--- shared/src/main/scala/de/thm/arsnova/shared/servicecommands/CommentCommands.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/servicecommands/CommentCommands.scala
@@ -6,11 +6,13 @@ import akka.Done
 import de.thm.arsnova.shared.entities.Comment
 
 object CommentCommands {
-  sealed trait CommentCommand extends ServiceCommand
+  sealed trait CommentCommand extends ServiceCommand {
+    def sessionId: UUID
+  }
 
-  case class GetComment(id: UUID) extends CommentCommand
+  case class GetComment(sessionId: UUID, id: UUID) extends CommentCommand
 
-  case class GetCommentBySessionId(id: UUID) extends CommentCommand
+  case class GetCommentsBySessionId(sessionId: UUID) extends CommentCommand
 
-  case class CreateComment(comment: Comment) extends CommentCommand
+  case class CreateComment(sessionId: UUID, comment: Comment) extends CommentCommand
 }
