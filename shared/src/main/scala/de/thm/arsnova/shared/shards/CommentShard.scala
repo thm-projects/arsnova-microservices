1cdecc8119ab8bcb3e87d457dd73754852f76ac8
Tom Käsler <tom.kaesler@mni.thm.de>
2017-08-09 14:13:07 +0200

    add commentshard

---
+++ shared/src/main/scala/de/thm/arsnova/shared/shards/CommentShard.scala
@@ -0,0 +1,18 @@
+package de.thm.arsnova.shared.shards
+import akka.cluster.sharding.ShardRegion
+import de.thm.arsnova.shared.events.SessionEventPackage
+import de.thm.arsnova.shared.servicecommands.CommentCommands.CommentCommand
+
+object CommentShard {
+  val shardName = "Comment"
+
+  val idExtractor: ShardRegion.ExtractEntityId = {
+    case cmd: CommentCommand => (cmd.sessionId.toString, cmd)
+    case event: SessionEventPackage => (event.id.toString, event)
+  }
+
+  val shardResolver: ShardRegion.ExtractShardId = {
+    case cmd: CommentCommand => math.abs(cmd.sessionId.hashCode() % 100).toString
+    case event: SessionEventPackage => math.abs(event.id.hashCode() % 100).toString
+  }
+}
