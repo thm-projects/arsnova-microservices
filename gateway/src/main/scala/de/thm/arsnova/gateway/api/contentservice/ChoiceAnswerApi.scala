389ae83eb75fc4b5074eda3e3628dfb6cd64ea3d
Tom Käsler <tom.kaesler@mni.thm.de>
2017-08-18 16:26:39 +0200

    update gateway

--- gateway/src/main/scala/de/thm/arsnova/gateway/api/contentservice/ChoiceAnswerApi.scala
+++ gateway/src/main/scala/de/thm/arsnova/gateway/api/contentservice/ChoiceAnswerApi.scala
@@ -2,6 +2,7 @@ package de.thm.arsnova.gateway.api.contentservice
 
 import java.util.UUID
 
+import scala.util.Try
 import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -9,40 +10,42 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.pattern.ask
 import akka.http.scaladsl.server.Directives._
 import spray.json._
-
 import de.thm.arsnova.gateway.api.BaseApi
 import de.thm.arsnova.shared.entities.ChoiceAnswer
 import de.thm.arsnova.shared.servicecommands.ChoiceAnswerCommands._
 import de.thm.arsnova.shared.servicecommands.CommandWithToken
+import de.thm.arsnova.gateway.sharding.AnswerListShard
 
 trait ChoiceAnswerApi extends BaseApi {
   import de.thm.arsnova.shared.mappings.ChoiceAnswerJsonProtocol._
 
+  val choiceAnswerListRegion = AnswerListShard.getProxy
+
   val choiceAnswerApi = pathPrefix("session") {
-    optionalHeaderValueByName("X-Session-Token") { tokenstring =>
-      pathPrefix(JavaUUID) { sessionId =>
-        pathPrefix("question") {
-          pathPrefix(JavaUUID) { questionId =>
-            pathPrefix("choiceanswer") {
-              pathPrefix(JavaUUID) { answerId =>
-                get {
-                  complete {
-                    (remoteCommander ? CommandWithToken(GetChoiceAnswer(answerId), tokenstring))
-                      .mapTo[ChoiceAnswer].map(_.toJson)
-                  }
-                }
-              } ~
+    pathPrefix(JavaUUID) { sessionId =>
+      pathPrefix("question") {
+        pathPrefix(JavaUUID) { questionId =>
+          pathPrefix("choiceanswer") {
+            pathPrefix(JavaUUID) { answerId =>
               get {
                 complete {
-                  (remoteCommander ? CommandWithToken(GetChoiceAnswersByQuestionId(questionId), tokenstring))
-                    .mapTo[Seq[ChoiceAnswer]].map(_.toJson)
+                  (choiceAnswerListRegion ? GetChoiceAnswer(sessionId, questionId, answerId))
+                    .mapTo[Try[ChoiceAnswer]]
                 }
-              } ~
-              post {
+              }
+            } ~
+            get {
+              complete {
+                (choiceAnswerListRegion ? GetChoiceAnswers(sessionId, questionId))
+                  .mapTo[Seq[ChoiceAnswer]].map(_.toJson)
+              }
+            } ~
+            post {
+              headerValueByName("X-Session-Token") { tokenstring =>
                 entity(as[ChoiceAnswer]) { answer =>
                   complete {
-                    (remoteCommander ? CommandWithToken(CreateChoiceAnswer(answer), tokenstring))
-                      .mapTo[Int].map(_.toJson)
+                    (choiceAnswerListRegion ? CreateChoiceAnswer(sessionId, questionId, answer, tokenstring))
+                      .mapTo[Try[ChoiceAnswer]].map(_.toJson)
                   }
                 }
               }
