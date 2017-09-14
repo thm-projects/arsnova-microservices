a7760222cdef7e4ea78e42b383e00893cdefa06f
Tom Käsler <tom.kaesler@mni.thm.de>
2017-09-14 08:26:43 +0200

    remove pubsub mechanism

--- managementservice/src/main/scala/de/thm/arsnova/managementservice/ServiceRegistryActor.scala
+++ managementservice/src/main/scala/de/thm/arsnova/managementservice/ServiceRegistryActor.scala
@@ -2,16 +2,11 @@ package de.thm.arsnova.managementservice
 
 import akka.cluster.{Cluster, Member}
 import akka.cluster.ClusterEvent._
-import akka.cluster.pubsub.DistributedPubSub
-import akka.cluster.pubsub.DistributedPubSubMediator.{Subscribe, SubscribeAck}
 import akka.actor.{Actor, ActorRef, ActorLogging, RootActorPath}
 import de.thm.arsnova.shared.management.RegistryCommands._
 
 class ServiceRegistryActor extends Actor with ActorLogging {
   val cluster = Cluster(context.system)
-
-  val mediator = DistributedPubSub(context.system).mediator
-  mediator ! Subscribe("registry", self)
 
   override def preStart(): Unit = {
     cluster.subscribe(self, classOf[MemberEvent], classOf[UnreachableMember])
@@ -41,9 +36,6 @@ class ServiceRegistryActor extends Actor with ActorLogging {
         // spread the dead service Actors
       }
     }
-
-    case SubscribeAck(Subscribe("registry", None, `self`)) =>
-      log.info("registry successfully subscribed to \"registry\"")
 
     // a service registers an actor for handling service commands
     case RegisterService(serviceType, remote) => {
