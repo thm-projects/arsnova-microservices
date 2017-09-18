dc05d8622b8717614987860e2a455605cefb8f7a
Tom Käsler <tom.kaesler@mni.thm.de>
2017-09-18 14:32:46 +0200

    remove unneccessary case class

--- shared/src/main/scala/de/thm/arsnova/shared/management/CommandPackage.scala
+++
@@ -1,8 +0,0 @@
-package de.thm.arsnova.shared.management
-
-import akka.actor.ActorRef
-
-import de.thm.arsnova.shared.servicecommands.ServiceCommand
-import de.thm.arsnova.shared.entities.User
-
-case class CommandPackage(command: ServiceCommand, user: Option[User], returnRef: ActorRef)
