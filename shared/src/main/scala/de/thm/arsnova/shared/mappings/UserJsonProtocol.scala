24f8dfd2c58ec6b3c8395809d73ade579de89e49
Tom Käsler <tom.kaesler@mni.thm.de>
2017-10-16 14:20:17 +0200

    fix name for userFormat

--- shared/src/main/scala/de/thm/arsnova/shared/mappings/UserJsonProtocol.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/mappings/UserJsonProtocol.scala
@@ -4,5 +4,5 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import de.thm.arsnova.shared.entities.User
 
 object UserJsonProtocol extends DefaultJsonProtocol with UUIDFormat {
-  implicit val commentFormat: RootJsonFormat[User] = jsonFormat3(User)
+  implicit val userFormat: RootJsonFormat[User] = jsonFormat3(User)
 }
