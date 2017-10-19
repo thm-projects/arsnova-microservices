8847eb97695cf17c93fb61c6e4841809c8e18e9f
Tom Käsler <tom.kaesler@mni.thm.de>
2017-10-19 15:24:25 +0200

    add json protocol for export entities

---
+++ shared/src/main/scala/de/thm/arsnova/shared/mappings/export/FreetextAnswerExportJsonProtocol.scala
@@ -0,0 +1,9 @@
+package de.thm.arsnova.shared.mappings.export
+
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import de.thm.arsnova.shared.entities.export.FreetextAnswerExport
+
+object FreetextAnswerExportJsonProtocol extends DefaultJsonProtocol {
+  implicit val freetextAnswerExportFormat: RootJsonFormat[FreetextAnswerExport] = jsonFormat2(FreetextAnswerExport)
+}
+
