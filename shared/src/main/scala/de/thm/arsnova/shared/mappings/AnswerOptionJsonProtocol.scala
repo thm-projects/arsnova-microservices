bfe3284a04145cc9659ecada5fda1c048aff670a
Tom Käsler <tom.kaesler@mni.thm.de>
2018-01-11 10:55:19 +0100

    fix protocols use class  instead of object

--- shared/src/main/scala/de/thm/arsnova/shared/mappings/AnswerOptionJsonProtocol.scala
+++ shared/src/main/scala/de/thm/arsnova/shared/mappings/AnswerOptionJsonProtocol.scala
@@ -4,5 +4,5 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import de.thm.arsnova.shared.entities.AnswerOption
 
 object AnswerOptionJsonProtocol extends DefaultJsonProtocol with UUIDFormat {
-  implicit val answerOptionFormat: RootJsonFormat[AnswerOption] = jsonFormat5(AnswerOption)
+  implicit val answerOptionFormat: RootJsonFormat[AnswerOption] = jsonFormat5(AnswerOption.apply)
 }
